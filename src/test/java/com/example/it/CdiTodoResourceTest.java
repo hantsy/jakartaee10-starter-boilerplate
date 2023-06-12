f1b776ddb71956a983f9644cf67abb6171684901
hantsy <hantsy@gmail.com>
2023-06-12 22:58:58 +0800

    chore: clean codes

--- src/test/java/com/example/it/CdiTodoResourceTest.java
+++ src/test/java/com/example/it/CdiTodoResourceTest.java
@@ -1,16 +1,14 @@
 package com.example.it;
 
-import com.example.cdi.CdiTodoRepository;
-import com.example.domain.Todo;
-import com.example.ejb.EjbTodoRepository;
-import com.example.rest.*;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.GenericType;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.net.URL;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -19,19 +17,31 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.net.URL;
-import java.util.List;
-import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import com.example.cdi.CdiTodoRepository;
+import com.example.domain.Todo;
+import com.example.ejb.EjbTodoRepository;
+import com.example.rest.CdiTodoResource;
+import com.example.rest.CreateTodoCommand;
+import com.example.rest.RestActivator;
+import com.example.rest.TodoNotFoundExceptionMapper;
+import com.example.rest.UpdateTodoCommand;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @ExtendWith(ArquillianExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CdiTodoResourceTest {
     private final static Logger LOGGER = Logger.getLogger(CdiTodoResourceTest.class.getName());
 
@@ -73,6 +83,7 @@ public class CdiTodoResourceTest {
     }
 
     @Test
+    @Order(1)
     public void testTodosAPI() throws Exception {
         LOGGER.log(Level.INFO, " Running test:: testTodosAPI ... ");
         final WebTarget allTodosTarget = client.target(new URL(baseUrl, "api/cditodos").toExternalForm());
@@ -86,6 +97,7 @@ public class CdiTodoResourceTest {
     }
 
     @Test
+    @Order(2)
     public void testTodoNotFound() throws Exception {
         LOGGER.log(Level.INFO, " Running test:: testTodoNotFound ... ");
         final WebTarget getByIdTarget = client
@@ -98,6 +110,7 @@ public class CdiTodoResourceTest {
     }
 
     @Test
+    @Order(3)
     public void testCreateTodoAPI() throws Exception {
         LOGGER.log(Level.INFO, " Running test:: testCreateTodoAPI ... ");
         final WebTarget createTodoTarget = client.target(new URL(baseUrl, "api/cditodos").toExternalForm());
